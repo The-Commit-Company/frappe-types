9633c91057da7159d52d44b773624ad25065f5c4
Sumit Jain <jainmit23@gmail.com>
2022-09-15 18:46:05 +0530

    Fix:Doctype Name change from App Settings to App Type Path Settings

--- frappe_types/frappe_types/doctype/app_type_generation_paths/app_type_generation_paths.py
+++ frappe_types/frappe_types/doctype/app_type_generation_paths/app_type_generation_paths.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class AppSettings(Document):
+class AppTypeGenerationPaths(Document):
 	pass
---
+++ frappe_types/frappe_types/doctype/type_generation_settings/test_type_generation_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Nikhil Kothari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTypeGenerationSettings(FrappeTestCase):
+	pass
