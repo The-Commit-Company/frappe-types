91c71737cafc81775fb5c452840ddca30ff1418d
Sumit Jain <jainmit23@gmail.com>
2022-09-15 18:52:16 +0530

    Fix:removed unnecesarry file

--- frappe_types/frappe_types/doctype/type_generation_settings/test_type_generation_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022, Nikhil Kothari and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTypeGenerationSettings(FrappeTestCase):
-	pass
