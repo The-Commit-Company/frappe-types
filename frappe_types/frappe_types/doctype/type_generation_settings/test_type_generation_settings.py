2f6afe882e502384f10b698ffd3fe04749218e9d
Sumit Jain <jainmit23@gmail.com>
2023-06-09 14:28:09 +0530

    feat:external app path

---
+++ frappe_types/frappe_types/doctype/type_generation_settings/test_type_generation_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Nikhil Kothari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTypeGenerationSettings(FrappeTestCase):
+	pass
