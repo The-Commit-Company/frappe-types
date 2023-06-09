4b27ba73d8839cfd8041c5582403a319c67f5426
Sumit Jain <jainmit23@gmail.com>
2023-06-09 14:28:50 +0530

    feat:generate type setting usinh command

--- frappe_types/frappe_types/type_generator.py
+++ frappe_types/frappe_types/type_generator.py
@@ -22,7 +22,7 @@ def create_type_definition_file(doc, method=None):
         print("Generating type definition file for " + doctype.name)
         module_name = doctype.module
         app_name = frappe.db.get_value('Module Def', module_name, 'app_name')
-        
+
         if app_name == "frappe" or app_name == "erpnext":
             print("Ignoring core app DocTypes")
             return
@@ -59,11 +59,11 @@ def generate_type_definition_file(doctype, module_path):
 
     doctype_name = doctype.name.replace(" ", "")
     type_file_path = module_path / (doctype_name + ".ts")
-    type_file_content = generate_type_definition_content(doctype)
+    type_file_content = generate_type_definition_content(doctype, module_path)
     create_file(type_file_path, type_file_content)
 
 
-def generate_type_definition_content(doctype):
+def generate_type_definition_content(doctype, module_path):
     generate_type_definition_content.imports = ""
     # print(generate_type_definition_content.imports)
     content = "export interface " + doctype.name.replace(" ", "") + "{\n"
@@ -75,7 +75,8 @@ def generate_type_definition_content(doctype):
         if field.fieldtype in ["Section Break", "Column Break", "HTML", "Button", "Fold", "Heading", "Tab Break", "Break"]:
             continue
         content += get_field_comment(field)
-        content += "\t" + get_field_type_definition(field, doctype) + "\n"
+        content += "\t" + \
+            get_field_type_definition(field, doctype, module_path) + "\n"
 
     content += "}"
     # print(generate_type_definition_content.imports)
@@ -90,11 +91,11 @@ def get_field_comment(field):
     return "\t/**\t" + field.label + " : " + field.fieldtype + ((" - " + desc) if desc else "") + "\t*/\n"
 
 
-def get_field_type_definition(field, doctype):
-    return field.fieldname + get_required(field) + ": " + get_field_type(field, doctype)
+def get_field_type_definition(field, doctype, module_path):
+    return field.fieldname + get_required(field) + ": " + get_field_type(field, doctype, module_path)
 
 
-def get_field_type(field, doctype):
+def get_field_type(field, doctype, module_path):
 
     basic_fieldtypes = {
         "Data": "string",
@@ -129,16 +130,19 @@ def get_field_type(field, doctype):
 
     if field.fieldtype in ["Table", "Table MultiSelect"]:
         # print(get_imports_for_table_fields(field, doctype))
-        return get_imports_for_table_fields(field, doctype)
+        return get_imports_for_table_fields(field, doctype, module_path)
 
     if field.fieldtype == "Select":
-        options = field.options.split("\n")
-        t = ""
-        for option in options:
-            t += "\"" + option + "\" | "
-        if t.endswith(" | "):
-            t = t[:-3]
-        return t
+        if (field.options):
+            options = field.options.split("\n")
+            t = ""
+            for option in options:
+                t += "\"" + option + "\" | "
+            if t.endswith(" | "):
+                t = t[:-3]
+            return t
+        else:
+            return 'string'
 
     if field.fieldtype in basic_fieldtypes:
         return basic_fieldtypes[field.fieldtype]
@@ -146,18 +150,37 @@ def get_field_type(field, doctype):
         return "any"
 
 
-def get_imports_for_table_fields(field, doctype):
+def get_imports_for_table_fields(field, doctype, module_path):
     if field.fieldtype == "Table" or field.fieldtype == "Table MultiSelect":
         doctype_module_name = doctype.module
         table_doc = frappe.get_doc('DocType', field.options)
         table_module_name = table_doc.module
 
+        # check if table doctype type file is already generated and exists
+
         if doctype_module_name == table_module_name:
+
+            table_file_path: Path = module_path / \
+                (table_doc.name.replace(" ", "") + ".ts")
+            if not table_file_path.exists():
+                generate_type_definition_file(table_doc, module_path)
+
             generate_type_definition_content.imports += ("import { " + field.options.replace(" ", "") + " } from './" +
                                                          field.options.replace(" ", "") + "'") + "\n"
 
             # print(generate_type_definition_content.imports)
         else:
+            table_module_path: Path = module_path.split(
+                "/").pop().join("/") / table_module_name.replace(" ", "")
+            if not table_module_path.exists():
+                table_module_path.mkdir()
+
+            table_file_path: Path = table_module_path / \
+                (table_doc.name.replace(" ", "") + ".ts")
+
+            if not table_file_path.exists():
+                generate_type_definition_file(table_doc, table_module_path)
+
             generate_type_definition_content.imports += ("import { " + field.options.replace(" ", "") + " } from '../" +
                                                          table_module_name.replace(" ", "") + "/" + field.options.replace(" ", "") + "'") + "\n"
             # print(generate_type_definition_content.imports)
@@ -202,3 +225,62 @@ def after_migrate():
     # print("After migrate")
     subprocess.run(["bench", "config", "set-common-config",
                    "-c", "frappe_types_pause_generation", "0"])
+
+
+@frappe.whitelist()
+def generate_types_for_doctype(doctype, app_name, generate_child_tables=False):
+    doc = frappe.get_meta(doctype)
+
+    # Check if type generation is paused
+    common_site_config = frappe.get_conf()
+
+    frappe_types_pause_generation = common_site_config.get(
+        "frappe_types_pause_generation", 0)
+
+    if frappe_types_pause_generation:
+        print("Frappe Types is paused")
+        return
+
+    doctype = doc
+
+    if is_developer_mode_enabled() and is_valid_doctype(doctype):
+        print("Generating type definition file for " + doctype.name)
+        module_name = doctype.module
+
+        app_path: Path = Path("../apps") / app_name
+        if not app_path.exists():
+            print("App path does not exist - ignoring type generation")
+            return
+
+        # Fetch Type Generation Settings Document
+        type_generation_settings = frappe.get_doc(
+            'Type Generation Settings'
+        ).as_dict().type_settings
+
+        # Checking if app is existed in type generation settings
+        for type_setting in type_generation_settings:
+            if app_name == type_setting.app_name:
+                # Types folder is created in the app
+                path = type_setting.external_app_path if type_setting.external_app_path else type_setting.app_path
+                type_path: Path = app_path / path / "types"
+
+                if not type_path.exists():
+                    type_path.mkdir()
+
+                module_path: Path = type_path / module_name.replace(" ", "")
+                if not module_path.exists():
+                    module_path.mkdir()
+
+                generate_type_definition_file(doctype, module_path)
+            else:
+                return
+
+
+@frappe.whitelist()
+def trial():
+    # TODO: Remove this function and use generate_child_tables parameter in generate_types_for_doctype
+    doctype = 'User'
+    app_name = 'my_asset_buddy'
+    generate_child_tables = True
+    generate_types_for_doctype(doctype, app_name, generate_child_tables)
+    return "Done"
